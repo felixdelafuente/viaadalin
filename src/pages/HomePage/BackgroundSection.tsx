444719f5e46fae5ea0f1bedd2e72cfdc5a35f2e3
Felix Jerome Dela Fuente <dlfuente.felixjerome@gmail.com>
2024-01-04 23:33:07 +0800

    fix: images not appearing pt.2

--- src/pages/HomePage/BackgroundSection.tsx
+++ src/pages/HomePage/BackgroundSection.tsx
@@ -2,7 +2,7 @@ export const BackgroundSection = () => {
   return (
     <section className='relative w-full px-4 md:px-16 lg:px-32 pt-4 md:pt-16 lg:pt-32 flex flex-col items-center overflow-hidden'>
       <img
-        src='public/images/up-stripes.png'
+        src='/images/up-stripes.png'
         alt=''
         className='z-20 hidden xl:block absolute right-0 -top-48 transform -rotate-45'
       />
@@ -11,11 +11,7 @@ export const BackgroundSection = () => {
       </h1>
       <div className='w-full mt-24 flex flex-col md:flex-row md:justify-center gap-8'>
         <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col items-center'>
-          <img
-            src='public/images/rot-1.png'
-            alt='First Rotation'
-            className='mb-4'
-          />
+          <img src='/images/rot-1.png' alt='First Rotation' className='mb-4' />
           <h4 className='text-2xl text-center font-recoletaBl'>
             Lorem Ipsum School
           </h4>
@@ -24,11 +20,7 @@ export const BackgroundSection = () => {
           </p>
         </div>
         <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col items-center'>
-          <img
-            src='public/images/rot-2.png'
-            alt='Second Rotation'
-            className='mb-4'
-          />
+          <img src='/images/rot-2.png' alt='Second Rotation' className='mb-4' />
           <h4 className='text-2xl text-center font-recoletaBl'>
             Lorem Ipsum School
           </h4>
@@ -37,11 +29,7 @@ export const BackgroundSection = () => {
           </p>
         </div>
         <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col items-center'>
-          <img
-            src='public/images/rot-0.png'
-            alt='Coming Soon'
-            className='mb-4'
-          />
+          <img src='/images/rot-0.png' alt='Coming Soon' className='mb-4' />
           <h4 className='text-2xl text-center font-recoletaBl'>
             Lorem Ipsum School
           </h4>
@@ -50,11 +38,7 @@ export const BackgroundSection = () => {
           </p>
         </div>
         <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col items-center'>
-          <img
-            src='public/images/rot-0.png'
-            alt='Coming Soon'
-            className='mb-4'
-          />
+          <img src='/images/rot-0.png' alt='Coming Soon' className='mb-4' />
           <h4 className='text-2xl text-center font-recoletaBl'>
             Lorem Ipsum School
           </h4>
